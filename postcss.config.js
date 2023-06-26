749860653680a624074d6e2cc695b2a3b09fff28
Hung Le <lehung.ibninc@gmail.com>
2023-06-26 08:59:31 +0700

    config tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
